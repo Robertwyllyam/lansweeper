fae2b3518062ff2f8ca34287dc55d6bee9813fa8
Robert Rocha <81169429+Robertwyllyam@users.noreply.github.com>
2022-06-30 16:35:46 -0300

    Add files via upload

---
+++ driver.ps1
@@ -0,0 +1,111 @@
+﻿# Checar se os drivers estão instalados
+
+$driverName = (gwmi win32_videocontroller).Name
+$processor = (gwmi win32_processor).name
+$ip = (Get-NetIPAddress | Where-Object PrefixOrigin -eq "dhcp").IPAddress
+
+
+$srv = "\\10.36.0.10\Suporte\RepoScritps_SD\LS"
+
+
+if($ip.Split('.')[1] -ne "36"){
+
+    
+    $srv = "\\10.33.4.159\DefaultPackageShare$"
+
+}else{
+
+
+}
+
+if(! (Test-Path C:\LansweeperLogs\Driver)){
+
+
+    mkdir -Path "C:\LansweeperLogs\Driver\"
+
+}
+
+
+
+$log_init = "Comecei a executar pelo menos. $processor"
+
+$log_init > c:\loginit.txt
+
+
+#Verificar geração do processador
+
+switch -Wildcard ($processor){
+    
+    "*650*"{$generation = "i5 650"}
+    "*-8*"{$generation = "7th"}
+    "*-7*"{$generation = "7th"}
+    "*-6*"{$generation = "7th"}
+    "*-6500*"{$generation = "7th"}
+    "*-4*"{$generation = "4th"}
+    "*-3*"{$generation = "3th"}
+    "*-2*"{$generation = "2th"}   
+    "*Duo*"{$generation = "Core2 Duo"}
+    
+
+}
+
+$path = "$srv\Installers\Office\drivers\$generation\setup.exe"
+
+
+$log += "$path
+
+
+        $generation
+    
+"
+
+
+
+"Caminho do srv $path"
+
+
+if($driverName -like "*Basic Display*" -or $driverName -eq $null -or $driverName -eq "Citrix Indirect Display Adapter	"){
+
+    
+    
+    if($generation -eq "7th"){
+    
+        cmd /c $path -s --report "C:\LansweeperLogs\Driver\log_driver.txt"
+
+    }else{
+    
+        
+        cmd /c $path -s -overwrite -report "C:\LansweeperLogs\Driver\"
+
+
+    }
+
+
+    
+  
+
+
+
+}else{
+    
+    $log += 
+    
+    "Já existe driver instalado nesta máquina!.
+    
+     Modelo: $processor
+
+     Driver: $driveRName
+
+     Geração: $generation
+
+
+    "
+
+    
+
+
+}
+
+
+
+$log > C:\LansweeperLogs\Driver\log_driver_deploy.txt
---
+++ unins.ps1
@@ -0,0 +1,71 @@
+﻿function desinstalarOffice($versao){
+
+    if($versao -eq '2019'){
+    
+            
+        \\srvlansweeper\DefaultPackageShare$\Installers\Office\2019\Desinstalar\setup.exe /configure \\srvlansweeper\DefaultPackageShare$\Installers\Office\2019\Desinstalar\config.xml
+            
+
+    }elseif($versao -eq '2016'){
+    
+        
+        \\srvlansweeper\Office\2016\setup.exe /uninstall Standard /config \\srvlansweeper\Office\2016\config_uninstall.xml
+
+    }elseif($versao -eq '2013'){
+    
+    
+        \\srvlansweeper\Office\2013\setup.exe /uninstall Standard /config \\srvlansweeper\Office\2013\config_uninstall.xml
+
+
+    }elseif($versao -eq '2010'){
+    
+        \\srvlansweeper\Office\2010\setup.exe /uninstall Standard /config \\srvlansweeper\Office\2010\config_uninstall.xml
+        
+
+    }else{
+    
+        return 'Nenhuma versão detectada na máquina'
+
+    }
+
+
+
+}
+
+## Destruir todos los processos del office antes :D"
+
+Get-Process | ? {($_.ProcessName -like '*office*' -or $_.ProcessName -like '*word*' -or $_.ProcessName -like '*excel*' -or $_.ProcessName -like '*outlook*' -or $_.ProcessName -like '*powerp*')} | Stop-Process -Force -Confirm:$false
+
+## Buscar se existe Office no Registro
+
+
+$32bit = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate, UninstallString
+
+$64bit = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate, UninstallString
+
+$total = $64bit + $32bit
+
+$versao
+
+$query = ($total | ? DisplayName -like 'Microsoft Office Standard*')[0].DisplayName
+
+
+
+
+$versao = switch -Wildcard ($query){
+
+    '*2016*' {'2016'}
+    '*2019*' {'2019'}
+    '*2013*' {'2013'}
+    '*2010*' {'2010'}
+}
+
+
+$64bit > C:\64bit.txt
+$query > C:\query.txt
+$versao > C:\versao.txt
+$32bit > C:\32bit.txt
+
+$exec = desinstalarOffice $versao
+
+$exec > C:\log_ls_office.txt
